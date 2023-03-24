a0ad8010ff5985aad2a7c44d4b82dcd2eeddafb5
Diki Armanda <diki.armada5@gmail.com>
2023-03-25 05:11:16 +0700

    release v1

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,14 +4,29 @@ let visual = 0, auditoring = 0, read = 0, kinesthetic = 0
 let indexSoal = 1, indexInput = 0, batas = 4
 let result = document.querySelectorAll('.result')
 
+function start() {
+    document.getElementById('home').classList.add('d-none')
+    document.getElementById('about').classList.add('d-none')
+    document.getElementById('contact').classList.add('d-none')
+    soal[0].classList.remove('d-none')
+}
+
 function next() {
     if (indexSoal == 15) {
         soal[0].classList.add('d-none')
         result[0].classList.remove('d-none')
-        result[1].innerHTML= `<h1>Visual = ${visual}</h1>
-                            <h1>Auditoring = ${auditoring}</h1>
-                            <h1>Read-Write = ${read}</h1>
-                            <h1>Kinesthetic = ${kinesthetic}</h1>`
+        result[1].innerHTML= `<tr><td scope="row">Visual</td>
+                                    <td>=</td>
+                                    <td>${visual}</td></tr>
+                                <tr><td scope="row">Auditoring</td>
+                                    <td>=</td>
+                                    <td>${auditoring}</td></tr>
+                                <tr><td scope="row">Read-Write</td>
+                                    <td>=</td>
+                                    <td>${read}</td></tr>
+                                <tr><td scope="row">Kinesthetic</td>
+                                    <td>=</td>
+                                    <td>${kinesthetic}</td></tr>`
     } else {
         for (indexInput; indexInput < batas; indexInput++) {
             if (input[indexInput].checked) {
@@ -38,8 +53,8 @@ function next() {
         soal[indexSoal].classList.remove('d-none')
         batas+=4
     }
-    console.log(visual);
-    console.log(auditoring);
-    console.log(read);
-    console.log(kinesthetic);
+}
+
+function reset() {
+    location.reload()
 }
